5fe77e25a8acf740ca594ea77ceed43b6db023b9
jdowni000 <jdownie@redhat.com>
2022-01-26 16:19:53 -0600

    errors, pushing for backup

--- go.mod
+++ go.mod
@@ -5,19 +5,20 @@ go 1.17
 require github.com/cristoper/gsheet v0.1.0
 
 require (
-	cloud.google.com/go v0.84.0 // indirect
+	cloud.google.com/go v0.100.2 // indirect
+	cloud.google.com/go/compute v0.1.0 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/googleapis/gax-go/v2 v2.0.5 // indirect
+	github.com/googleapis/gax-go/v2 v2.1.1 // indirect
 	github.com/integrii/flaggy v1.4.4 // indirect
 	go.opencensus.io v0.23.0 // indirect
 	golang.org/x/net v0.0.0-20210503060351-7fd8e65b6420 // indirect
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
-	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	golang.org/x/text v0.3.6 // indirect
-	google.golang.org/api v0.49.0 // indirect
+	google.golang.org/api v0.65.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto v0.0.0-20210617175327-b9e0b3197ced // indirect
-	google.golang.org/grpc v1.38.0 // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
+	google.golang.org/genproto v0.0.0-20220107163113-42d7afdf6368 // indirect
+	google.golang.org/grpc v1.40.1 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 )
--- json_csv_google_sheets.go
+++ json_csv_google_sheets.go
@@ -6,7 +6,6 @@ import (
 	"encoding/json"
 	"flag"
 	"io"
-	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
@@ -20,9 +19,10 @@ var json_file_path string
 var csv_file_name string
 var google_sheet_name string
 var google_parent_id string
+var reader io.Reader
 
-// Output struct
-type PL_JsonStruct struct {
+// Struct specifically for Pod Latency summary json files
+type PodLatencyStruct struct {
 	QuantileName string "json:'quantileName'"
 	UUID         string "json:'uuid'"
 	P99          int    "json:'p99'"
@@ -35,13 +35,20 @@ type PL_JsonStruct struct {
 	JobName      string "json:'jobName'"
 }
 
+// Default strcut to use for json files
 type JsonStruct struct {
 	Timestamp  string "json:'timestamp'"
+	Labels     Labels "json:'labels'"
 	Value      int    "json:'value'"
 	UUID       string "json:'uuid'"
 	Query      string "json:'query'"
 	MetricName string "json:'metricName'"
 	JobName    string "json:'jobName'"
+}
+
+//struct for nested object in JsonStruct
+type Labels struct {
+	Node string "json:'instance'"
 }
 
 func init() {
@@ -69,147 +76,167 @@ func init() {
 	if google_parent_id == "" {
 		log.Fatal("Please provide google sheets parent folder id using flag '-p'")
 	}
+
 }
 
 func main() {
-	err := convert_json_to_csv(json_file_path, csv_file_name)
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	// sheet_id, err := create_google_sheet("test-Friday", "./test-web-burner-8a2dce06046f.json")
-	err = write_to_google_sheet(google_sheet_name+".csv", google_parent_id, "./"+csv_file_name)
-	if err != nil {
-		log.Fatal(err)
-	}
+	//Determine present working directory
+	wd, err := os.Getwd()
+	error_check(err)
+
+	//create csv file
+	csv_file, err := create_csv(wd, csv_file_name)
+	error_check(err)
+
+	//create google sheet
+	sheed_id, err := create_gs(google_sheet_name, google_parent_id)
+	error_check(err)
+
+	//unmarshal json into csv file
+	struct_req := json_identifier(json_file_path)
+
+	//upload csv to google sheets
+
 }
 
-func convert_json_to_csv(source string, destination string) error {
+//func create_csv creates a csv file
+func create_csv(wd string, csv_file_name string) (*os.File, error) {
+	var empty *os.File
+	csv_file_name = csv_file_name + ".csv"
+	//Delete csv file if it exists
+	_, err := os.Stat(wd + "/" + csv_file_name)
+	if err == nil {
+		log.Println("CSV filename " + csv_file_name + " already exists: Removing existing file before proceeding!")
+		err = os.Remove(csv_file_name)
+		if err != nil {
+			return empty, err
+		}
+	}
+	//create csv file
+	file, err := os.Create(csv_file_name)
+	if err != nil {
+		return empty, err
+	}
+	defer file.Close()
+	return file, nil
+}
 
-	// Read the JSON file into the struct array
-	data, err := ioutil.ReadFile(source)
+//func json_to_csv takes a json file and unmarshalls it to a defined csv file
+func json_to_csv(json_file string, struct_req string, csv_file *os.File) error {
+
+	w := csv.NewWriter(csv_file)
+
+	if struct_req == "pod_latency_struct" {
+		var pl []PodLatencyStruct
+		//unmarshal data
+		err := json.Unmarshal([]byte(json_file), &pl)
+		if err != nil {
+		}
+		//header := []string{"timestamp", "value", "uuid", "query", "metricName", "jobName"}
+		// Clear up logic exactly what Murali wants here for csv file format
+		//code....s
+		//err = w.Write(header)
+
+	}
+	var js []JsonStruct
+	//unmarshal data
+	err := json.Unmarshal([]byte(json_file), &js)
 	if err != nil {
 		return err
-	}
-
-	//Determing which struct to use and unmarshal json
-	var pl []PL_JsonStruct
-	var js []JsonStruct
-	var header []string
-	wd, err := os.Getwd()
-	if err != nil {
-		log.Fatal("Could not find pwd to check for existing files! Aboritng!")
-	}
-
-	if strings.Contains(json_file_path, "job-podLatency-summary.json") {
-		//unmarshal data
-		err = json.Unmarshal([]byte(data), &pl)
-		if err != nil {
-			return err
-		}
-
-		//Delete csv file if it exists and create a new one
-		_, err := os.Stat(wd + "/" + destination)
-		if err == nil {
-			log.Println("CSV filename " + destination + " already exists: Removing file before proceeding!")
-			err = os.Remove(destination)
-			if err != nil {
-				log.Fatal("Failed to remove CSV file with the same name as requested. Terminating to ensure accurate data")
-			}
-		}
-		file, err := os.Create(destination)
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-
-		//Write json data to csv file
-		header = []string{"quantileName", "uuid", "p99", "p95", "p50", "max", "avg", "timestamp", "metricName", "jobName"}
-		w := csv.NewWriter(file)
-		err = w.Write(header)
-		if err != nil {
-			return err
-		}
-
-		for _, o := range pl {
-			var csvRow []string
-			csvRow = append(csvRow, o.QuantileName, o.UUID, strconv.Itoa(o.P99), strconv.Itoa(o.P95), strconv.Itoa(o.P50), strconv.Itoa(o.Max), strconv.Itoa(o.Avg), o.Timestamp, o.MetricName, o.JobName)
-			err := w.Write(csvRow)
-			if err != nil {
-				return err
-			}
-		}
-		w.Flush()
-	} else {
-		//unmarshal data
-		err = json.Unmarshal([]byte(data), &js)
-		if err != nil {
-			return err
-		}
-
-		//Delete csv file if it exists and create a new one
-		_, err := os.Stat(wd + "/" + destination)
-		if err == nil {
-			log.Println("CSV filename " + destination + " already exists: Removing file before proceeding!")
-			err = os.Remove(destination)
-			if err != nil {
-				log.Fatal("Failed to remove CSV file with the same name as requested. Terminating to ensure accurate data")
-			}
-		}
-		file, err := os.Create(destination)
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-
-		//Write json data to csv file
-		header = []string{"timestamp", "value", "uuid", "query", "metricName", "jobName"}
-		w := csv.NewWriter(file)
-		err = w.Write(header)
-		if err != nil {
-			return err
-		}
-
-		log.Println("TEST TEST TEST")
-
-		for _, o := range js {
-			var csvRow []string
-			csvRow = append(csvRow, o.Timestamp, strconv.Itoa(o.Value), o.UUID, o.Query, o.MetricName, o.JobName)
-			err := w.Write(csvRow)
-			if err != nil {
-				return err
-			}
-		}
-		w.Flush()
 	}
 	return nil
 }
 
-func write_to_google_sheet(sheet_name string, parent string, csv_file string) error {
+//
+func data_calc(j []JsonStruct) {
+	var job_name []string
+	var node []string
+	var csv_row []string
+	max := 0
 
+	for _, jn := range j {
+		if !exists(job_name, jn.JobName) {
+			job_name = append(job_name, jn.JobName)
+		}
+	}
+
+	for _, n := range j {
+		if !exists(node, n.Labels.Node) {
+			node = append(node, n.Labels.Node)
+		}
+	}
+
+	for _, c := range j {
+		if !exists(job_name, c.JobName) || !exists(node, c.Labels.Node) {
+			csv_row = append(csv_row, c.JobName, c.Labels.Node, //cvalue max)
+		}
+	}
+
+	for _, value := range j {
+		if value.Value > max {
+			max = value.Value
+			csv_row = append(csv_row, value.JobName, value.Labels.Node, strconv.Itoa(max))
+
+		}
+	}
+
+}
+
+//func exists checks if an element exists againnt an array
+func exists(a []string, element string) bool {
+	for _, e := range a {
+		if e == element {
+			return true
+		}
+	}
+	return false
+}
+
+//func json_identifier determines what json file is being used to pass in correct struct for unmarsahlling
+func json_identifier(json_file string) string {
+
+	var json_struct_req string
+
+	if strings.Contains(json_file, "job-podLatency-summary.json") {
+		json_struct_req = "pod_latency_struct"
+		return json_struct_req
+	}
+	json_struct_req = "json_struct"
+	return json_struct_req
+}
+
+//func create_gs creates a new google spreadsheet
+func create_gs(sheet_name string, parent string) (string, error) {
 	var r io.Reader
 
 	gdrive_srv, err := gdrive.NewServiceWithCtx(context.TODO())
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	new_sheet, err := gdrive_srv.CreateFile(sheet_name, parent, r)
 	if err != nil {
-		return err
+		return "", err
 	}
+
+	return new_sheet.Id, nil
+}
+
+//func write_to_google_sheets creates a specified google sheet utilizing an existing csv file
+func write_to_google_sheet(csv_file string, sheet_name string, parent string, sheet_id string) error {
 
 	gsheet_srv, err := gsheets.NewServiceWithCtx(context.TODO())
 	if err != nil {
 		return err
 	}
 
-	r, err = os.Open(csv_file)
+	r, err := os.Open(csv_file)
 	if err != nil {
 		return err
 	}
 
-	resp, err := gsheet_srv.UpdateRangeCSV(new_sheet.Id, "A001", r)
+	resp, err := gsheet_srv.UpdateRangeCSV(sheet_id, "A001", r)
 	if err != nil {
 		return err
 	}
@@ -217,10 +244,18 @@ func write_to_google_sheet(sheet_name string, parent string, csv_file string) er
 	return nil
 }
 
+//func derefString removes the pointer to a string
 func derefString(s *string) string {
 	if s != nil {
 		return *s
 	}
 
 	return ""
+}
+
+//func error checks error and exits if is not empty
+func error_check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
