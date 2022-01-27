c346f0ab77cdcbbb01317cdc8247f4e6acd1c03f
jdowni000 <jdownie@redhat.com>
2022-01-27 16:29:31 -0600

    added formatting by job/node

--- json_csv_google_sheets.go
+++ json_csv_google_sheets.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"flag"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
@@ -23,33 +24,42 @@ var reader io.Reader
 
 // Struct specifically for Pod Latency summary json files
 type PodLatencyStruct struct {
-	QuantileName string "json:'quantileName'"
-	UUID         string "json:'uuid'"
-	P99          int    "json:'p99'"
-	P95          int    "json:'p95'"
-	P50          int    "json:'p50'"
-	Max          int    "json:'max'"
-	Avg          int    "json:'avg'"
-	Timestamp    string "json:'timestamp'"
-	MetricName   string "json:'metricName'"
-	JobName      string "json:'jobName'"
+	QuantileName string `json:"quantileName"`
+	UUID         string `json:"uid"`
+	P99          int    `json:"p99"`
+	P95          int    `json:"p95"`
+	P50          int    `json:"p50"`
+	Max          int    `json:"max"`
+	Avg          int    `json:"avg"`
+	Timestamp    string `json:"timestamp"`
+	MetricName   string `json:"metricName"`
+	JobName      string `json:"jobName"`
 }
 
 // Default strcut to use for json files
 type JsonStruct struct {
-	Timestamp  string "json:'timestamp'"
-	Labels     Labels "json:'labels'"
-	Value      int    "json:'value'"
-	UUID       string "json:'uuid'"
-	Query      string "json:'query'"
-	MetricName string "json:'metricName'"
-	JobName    string "json:'jobName'"
+	Timestamp  string `json:"timestamp"`
+	Labels     Inner  `json:"labels"`
+	Value      int    `json:"value"`
+	UUID       string `json:"uuid"`
+	Query      string `json:"query"`
+	MetricName string `json:"metricName"`
+	JobName    string `json:"jobName"`
 }
 
-//struct for nested object in JsonStruct
-type Labels struct {
-	Node string "json:'instance'"
+type Inner struct {
+	Node string `json:"instance"`
 }
+
+// type maxValue struct {
+// 	JobName    string
+// 	Node       string
+// 	MetricName string
+// 	Value      string
+// 	Timestamp  string
+// 	UUID       string
+// 	Query      string
+// }
 
 func init() {
 
@@ -85,24 +95,23 @@ func main() {
 	wd, err := os.Getwd()
 	error_check(err)
 
-	//create csv file
-	csv_file, err := create_csv(wd, csv_file_name)
-	error_check(err)
-
-	//create google sheet
-	sheed_id, err := create_gs(google_sheet_name, google_parent_id)
-	error_check(err)
-
-	//unmarshal json into csv file
+	// //unmarshal json into csv file
 	struct_req := json_identifier(json_file_path)
 
-	//upload csv to google sheets
+	//create csv file
+	err = create_csv(wd, csv_file_name, json_file_path, struct_req)
+	error_check(err)
+
+	// //create google sheet
+	// sheed_id, err := create_gs(google_sheet_name, google_parent_id)
+	// error_check(err)
+
+	// //upload csv to google sheets
 
 }
 
 //func create_csv creates a csv file
-func create_csv(wd string, csv_file_name string) (*os.File, error) {
-	var empty *os.File
+func create_csv(wd string, csv_file_name string, json_file string, struct_req string) error {
 	csv_file_name = csv_file_name + ".csv"
 	//Delete csv file if it exists
 	_, err := os.Stat(wd + "/" + csv_file_name)
@@ -110,22 +119,33 @@ func create_csv(wd string, csv_file_name string) (*os.File, error) {
 		log.Println("CSV filename " + csv_file_name + " already exists: Removing existing file before proceeding!")
 		err = os.Remove(csv_file_name)
 		if err != nil {
-			return empty, err
+			return err
 		}
+		log.Println("Existing CSV file removed with the same name")
 	}
 	//create csv file
 	file, err := os.Create(csv_file_name)
 	if err != nil {
-		return empty, err
+		return err
 	}
 	defer file.Close()
-	return file, nil
+
+	w := csv.NewWriter(file)
+
+	log.Println("CSV file created")
+
+	log.Println("Begining to retrieve data from json file to write to CSV file")
+	err = json_to_csv(json_file, struct_req, w)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 //func json_to_csv takes a json file and unmarshalls it to a defined csv file
-func json_to_csv(json_file string, struct_req string, csv_file *os.File) error {
+func json_to_csv(json_file string, struct_req string, w *csv.Writer) error {
 
-	w := csv.NewWriter(csv_file)
+	data, err := ioutil.ReadFile(json_file)
 
 	if struct_req == "pod_latency_struct" {
 		var pl []PodLatencyStruct
@@ -133,55 +153,158 @@ func json_to_csv(json_file string, struct_req string, csv_file *os.File) error {
 		err := json.Unmarshal([]byte(json_file), &pl)
 		if err != nil {
 		}
-		//header := []string{"timestamp", "value", "uuid", "query", "metricName", "jobName"}
-		// Clear up logic exactly what Murali wants here for csv file format
-		//code....s
-		//err = w.Write(header)
+		return err
 
 	}
 	var js []JsonStruct
 	//unmarshal data
-	err := json.Unmarshal([]byte(json_file), &js)
+	err = json.Unmarshal([]byte(data), &js)
 	if err != nil {
 		return err
 	}
+
+	err = data_calc(js, w)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 //
-func data_calc(j []JsonStruct) {
-	var job_name []string
-	var node []string
-	var csv_row []string
-	max := 0
+func data_calc(jstruct []JsonStruct, w *csv.Writer) error {
 
-	for _, jn := range j {
-		if !exists(job_name, jn.JobName) {
-			job_name = append(job_name, jn.JobName)
+	header := []string{"JobName", "Node", "MaxValue", "MetricName", "Timestamp", "UUID", "Query"}
+	w.Write(header)
+	w.Flush()
+	var all_job_names []string
+	var all_node_names []string
+
+	// var job_name []string
+
+	for _, all := range jstruct {
+		if !(exists(all_job_names, all.JobName)) {
+			all_job_names = append(all_job_names, all.JobName)
 		}
 	}
 
-	for _, n := range j {
-		if !exists(node, n.Labels.Node) {
-			node = append(node, n.Labels.Node)
+	for _, node := range jstruct {
+		if !(exists(all_node_names, node.Labels.Node)) {
+			all_node_names = append(all_node_names, node.Labels.Node)
 		}
 	}
 
-	for _, c := range j {
-		if !exists(job_name, c.JobName) || !exists(node, c.Labels.Node) {
-			csv_row = append(csv_row, c.JobName, c.Labels.Node, //cvalue max)
+	count_nodes := len(all_node_names)
+
+	n := 0
+
+	for n < count_nodes {
+
+		var jobs_by_node []string
+
+		for _, v := range jstruct {
+			if v.Labels.Node == all_node_names[n] && !(exists(jobs_by_node, v.JobName)) {
+				jobs_by_node = append(jobs_by_node, v.JobName)
+			}
 		}
-	}
 
-	for _, value := range j {
-		if value.Value > max {
-			max = value.Value
-			csv_row = append(csv_row, value.JobName, value.Labels.Node, strconv.Itoa(max))
+		var csv_row []string
+		num_jobs := len(jobs_by_node)
+		j := 0
+		max := 0
 
+		for j < num_jobs {
+			var temp []string
+			for _, v := range jstruct {
+				if v.Labels.Node == all_node_names[n] && v.JobName == jobs_by_node[j] {
+					if v.Value > max {
+						max = v.Value
+						temp = []string{v.JobName, v.Labels.Node, strconv.Itoa(v.Value), v.MetricName, v.Timestamp, v.UUID, v.Query}
+					}
+				}
+			}
+			csv_row = temp
+			w.Write(csv_row)
+			w.Flush()
+			j++
 		}
+		n++
 	}
-
+	return nil
 }
+
+// n := 0
+// j := 0
+// max := 0
+
+//if node name equals this
+//and job name equals this
+//find values
+//append csv_row
+//write row to csv
+
+// for n < count_nodes {
+// 	// var csv_row []string
+// 	var jobs_by_node []string
+// 	for _, n := range jstruct {
+// 		if n.Labels.Node == all_node_names[n] {
+// 			for
+// 		}
+// 	}
+
+// 	num_jobs := len(jobs_by_node)
+// 	log.Println(num_jobs)
+// 	log.Println(jobs_by_node)
+
+// i := 0
+// max := 0
+// for i < num_jobs {
+// 	for _, v := range jstruct {
+// 		if v.JobName == jobs_by_node[i] || v.Value > max {
+// 			max = v.Value
+// 			csv_row = append(csv_row, v.JobName, v.Labels.Node, strconv.Itoa(v.Value), v.MetricName, v.Timestamp, v.UUID, v.Query)
+// 		}
+// 		log.Println(csv_row)
+// 		w.Write(csv_row)
+// 		w.Flush()
+// 	}
+// 	i++
+// }
+
+// 	n++
+// 	w.Flush()
+// }
+
+// 	if v.Value > max {
+// 		max = v.Value
+// 		temp = []string{v.JobName, v.Labels.Node, strconv.Itoa(v.Value), v.MetricName, v.Timestamp, v.UUID, v.Query}
+// 	}
+// 	return nil
+// }
+
+// for j < count_jobs {
+
+// 	var temp_csv []string
+// 	for _, jn := range jstruct {
+// 		if jn.JobName == all_job_names[j] {
+// 			if jn.Value > max {
+// 				max = jn.Value
+// 				temp_csv = []string{jn.JobName, jn.Labels.Node, strconv.Itoa(jn.Value), jn.MetricName, jn.Timestamp, jn.UUID, jn.Query}
+// 			}
+// 		}
+// 	}
+// 	header := []string{"JobName", "Node", "MaxValue", "MetricName", "Timestamp", "UUID", "Query"}
+// 	err := w.Write(header)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	err = w.Write(temp_csv)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	max = 0
+
+// }
 
 //func exists checks if an element exists againnt an array
 func exists(a []string, element string) bool {
